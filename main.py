92fcfad73c2f0b1c34c20db53f9ee85f0f21f678
ygrz213 <yigitgurbuz0630@gmail.com>
2022-04-10 19:34:30 +0300

    Ödünç verilmiş kitaplar butonu eklendi

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ from ttkthemes import ThemedTk
 root = ThemedTk(theme = 'arc')
 root.wm_iconbitmap('icons/book.ico')
 
+button_frame = tk.Frame()
+button_frame.pack(side = 'top', fill = 'x')
+
 add_book_icon = tk.PhotoImage(file = 'icons/plus.png')
-add_book = ttk.Button(text = '   Kitap ekle', image = add_book_icon, compound = 'left')
-add_book.grid(padx = (35,0), ipady = 3, ipadx = 1)
+add_book = ttk.Button(button_frame, text = '   Kitap ekle', image = add_book_icon, compound = 'left')
+add_book.pack(side = 'left', ipadx = 3, ipady = 1)
+
+lent_books_icon = tk.PhotoImage(file = 'icons/lend.png')
+lent_books = ttk.Button(button_frame, text = 'Ödünç verilmiş kitaplar', image = lent_books_icon, compound = 'top')
+lent_books.pack(side = 'top', ipadx = 3, ipady = 1)
+
 
 root.mainloop()
